8847ffe4dd64a33ac23aaa72a7c8f32250ccdefb
Ronald Diaz <ronald@ronalddiaz.net>
2013-02-03 19:07:16 +1000

    updated setup description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='ronniedAmp',
       version='1.0',
-      description='Python library for interfacing custom network controlled i2c controlled amplifier',
+      description='Python library for interfacing custom network (i2c controlled) amplifier',
       author='Ronald Diaz',
       author_email='ronald@ronalddiaz.net',
       url='http://github.com/ronnied/network-amplifier/',
